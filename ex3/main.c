b6cd4c22fe54e0dcfb268e590c7b67928635d4e3
Live session user <vincent.surkijn@student.kuleuven.be>
2020-10-07 16:33:47 +0000

    First working version sensor

--- ex3/main.c
+++ ex3/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
 #define FREQUENCY 5
 #define MIN (-10)
@@ -8,12 +9,26 @@
 
 int main(){
 
+float temp;
+
+char buffer[80];
+time_t rawtime;
+struct tm *info;
+time( &rawtime );
+info = gmtime( &rawtime );
+strftime(buffer,80,"%a %d %b %Y %I:%M:%S %p %Z",info);
+
 srand(time(NULL));
 
-printf("%d\n",(int) ( (( (double)(MAX+1) - (double)(MIN-1) )*rand()/(RAND_MAX+1.0)) + (double)(MIN-1) ));
-printf("%d\n",(int) ( (( (double)(MAX+1) - (double)(MIN-1) )*rand()/(RAND_MAX+1.0)) + (double)(MIN-1) ));
-printf("%d\n",(int) ( (( (double)(MAX+1) - (double)(MIN-1) )*rand()/(RAND_MAX+1.0)) + (double)(MIN-1) ));
-printf("%d\n",(int) ( (( (double)(MAX+1) - (double)(MIN-1) )*rand()/(RAND_MAX+1.0)) + (double)(MIN-1) ));
+//Format date: Wed 07 Oct 2020 04:02:10 PM UTC
+//printf("%d\n",(int) ( (( (double)(MAX+1) - (double)(MIN-1) )*rand()/(RAND_MAX+1.0)) + (double)(MIN-1) ));
+
+while(1){
+	temp =  ( (( (double)(MAX+1) - (double)(MIN-1) )*rand()/(RAND_MAX+1.0)) + (double)(MIN-1));
+	printf("Temperature = %1.2f @ %s \n", temp, buffer);
+	fflush(stdout);
+	sleep(FREQUENCY);
+}
 
 return 0;
 }
