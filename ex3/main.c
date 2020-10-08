7bddc8074df6e1f5ea59d3971d23e77b2ca6db79
Live session user <vincent.surkijn@student.kuleuven.be>
2020-10-08 09:26:40 +0000

    Tried to reduce scope of temp and made date change for every new input

--- ex3/main.c
+++ ex3/main.c
@@ -11,24 +11,25 @@ int main(){
 
 float temp;
 
+//Variables for printing date in right format
 char buffer[80];
 time_t rawtime;
 struct tm *info;
-time( &rawtime );
-info = gmtime( &rawtime );
-strftime(buffer,80,"%a %d %b %Y %I:%M:%S %p %Z",info);
 
 srand(time(NULL));
 
 //Format date: Wed 07 Oct 2020 04:02:10 PM UTC
-//printf("%d\n",(int) ( (( (double)(MAX+1) - (double)(MIN-1) )*rand()/(RAND_MAX+1.0)) + (double)(MIN-1) ));
 
 while(1){
-	temp =  ( (( (double)(MAX+1) - (double)(MIN-1) )*rand()/(RAND_MAX+1.0)) + (double)(MIN-1));
+	time( &rawtime );
+	info = gmtime( &rawtime );
+	strftime(buffer,80,"%a %d %b %Y %I:%M:%S %p %Z",info);
+
+	temp = ((double)rand() * (MAX-MIN))/(double)RAND_MAX+MIN;
 	printf("Temperature = %1.2f @ %s \n", temp, buffer);
 	fflush(stdout);
 	sleep(FREQUENCY);
-}
+	}
 
 return 0;
 }
