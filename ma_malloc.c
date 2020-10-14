545202c62df5250e15820e66ca911aa8b827a8a2
Live session user <vincent.surkijn@student.kuleuven.be>
2020-10-14 11:59:13 +0200

    ma_alloc() kind of works, need to find out how to clear mem_pool in init()

--- ma_malloc.c
+++ ma_malloc.c
@@ -23,8 +23,6 @@ typedef struct {
 
 static byte mem_pool[MEM_POOL_SIZE];
 
-mem_chunk_header* h_addresses [MEM_POOL_SIZE];
-
 /**
  * Allocates array of bytes (memory pool) and initializes the memory allocator.
  * If some bytes have been used after calling ma_malloc(size), calling to ma_init() will result in clearing up the memory pool.
@@ -35,41 +33,34 @@ void ma_init() {
     first = (mem_chunk_header*) mem_pool;	// mem_pool = &mem_pool[0]
     first->status = FREE;	// (*first).status = FREE;
     first->size = MEM_POOL_SIZE - sizeof(mem_chunk_header);	//don't use variable in sizeof()
-//Save address of first header
-    h_addresses[0]=(mem_chunk_header*) mem_pool;
+    printf("Size of first header: %d\n", first->size);
 }
+
 /**
  * Requesting for the tsize bytes from memory pool.
  * If the request is possible, the pointer to the first possible address byte (right after its header) in memory pool is returned.
  */
 void *ma_malloc(size tsize) {
-//Retrieve first free header and set status + size 
-    int i = 0;
-    while(h_addresses[i]->status=ALLOCATED){
+//Retrieve first free header and set status
+    printf("TEst\n");
+    mem_chunk_header* first = (mem_chunk_header*)mem_pool;
+    int i =0;
+    while(first->status==ALLOCATED && first->size>0){ //Mem_pool has to be cleared or random positive values can still be there!
+	first = (mem_chunk_header*)(((byte*)first) + sizeof(mem_chunk_header) + first->size);
+	printf("%d\n",i);
 	i++;
+	printf("Status of header: %d\n",first->status);
     }
-    mem_chunk_header* first;
-    first = h_addresses[i];
     first->status=ALLOCATED;
-    first->size=tsize;
-//Set status and size of next header
-    mem_chunk_header* new_header;
-    new_header = (mem_chunk_header*)(((byte*)first) + sizeof(mem_chunk_header) + tsize);
-    new_header->status=FREE;
-    new_header->size=MEM_POOL_SIZE-sizeof(mem_chunk_header)-sizeof(first);//Still need to include size of previous occupied headers
-	//What when occupied spaces after first free space?
-
-//Retrieve first header and set status + size
-/**    mem_chunk_header* first;
-    first = h_addresses[0];
-    first->status=ALLOCATED;
-    first->size=tsize;
 //Set status and size of next header
     mem_chunk_header* new_header;
     new_header = (mem_chunk_header*)(((byte*)first) + sizeof(mem_chunk_header) + first->size);	//mem location of new header
     new_header->status = FREE;
-    new_header->size=MEM_POOL_SIZE-2*sizeof(mem_chunk_header)-tsize;
-*/
+    new_header->size=first->size - sizeof(mem_chunk_header) - tsize;
+//Now set size of first free header
+    first->size=tsize;
+//Return pointer to allocated memory
+    return (byte*)first + sizeof(mem_chunk_header);
 }
 
 /**
@@ -90,6 +81,7 @@ void ma_print(void) {
     mem_chunk_header* i = (mem_chunk_header*)mem_pool;
     mem_chunk_header* last = (mem_chunk_header*)mem_pool + MEM_POOL_SIZE;
     while(i<last){
+//	printf("Address: %p\n",(void*)i);
 	printf("%d\n", i->size);
 	i++;
     }
@@ -97,6 +89,9 @@ void ma_print(void) {
 
 //Debug:
 int main(){
+	ma_init();
 	ma_malloc(10);
-	ma_print();
+	ma_malloc(20);
+	ma_malloc(15);
+//	ma_print();
 }
