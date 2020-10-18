994f0c0db0da5b9d46251c0e05e70482a64df381
Live session user <vincent.surkijn@student.kuleuven.be>
2020-10-18 16:48:14 +0200

    First version of coalescing behaviour implemented, main.c gives no errors
    But:
    Weird behaviour when freeing first/last chunk

--- ma_malloc.c
+++ ma_malloc.c
@@ -56,13 +56,16 @@ void ma_init() {
  * If the request is possible, the pointer to the first possible address byte (right after its header) in memory pool is returned.
  */
 void *ma_malloc(size tsize) {
-//printf("Requested size: %d\n", tsize);
 //Retrieve first free header and set status
     printf("ma_malloc called\n");
     mem_chunk_header* first;
     mem_chunk_header* last;
+    mem_chunk_footer* foot;
+
     first  = (mem_chunk_header*)mem_pool;
     last = (mem_chunk_header*)((byte*)first + MEM_POOL_SIZE);
+    foot = (mem_chunk_footer*)((byte*)first +sizeof(mem_chunk_header) + first->size);
+
     int i =0;
     int size_needed = tsize + sizeof(mem_chunk_header) + sizeof(mem_chunk_footer);
     while(first->status==ALLOCATED || (first->size)<size_needed ){
@@ -72,16 +75,21 @@ void *ma_malloc(size tsize) {
 	    printf("Not enough space left!\n");
 	    return NULL;
 	}
-/** Debug*/
+/** Debug
 	printf("Loop %d\n", i);
 	printf("Status of header in loop: %d\n",first->status);
 	printf("Size of header in loop: %d\n",first->size);
         printf("Address of header in loop: %p\n",first);
   /**/  }
-    printf("Amount of loops: %d\n",i);
+    //printf("Amount of loops: %d\n",i);
     int temp_size = first->size;
+//Set status and size of current header and footer
     first->status=ALLOCATED;
     first->size=tsize;
+    foot = (mem_chunk_footer*)((byte*)first +sizeof(mem_chunk_header) + tsize);
+//printf("About to set current footer at address: %p\n",foot);
+    foot->status=first->status;
+    foot->size=first->size;
 //Set status and size of next header
     mem_chunk_header* new_header;	//New_header is the header that comes right after the newly allocated chunk
     new_header = (mem_chunk_header*)(((byte*)first) + sizeof(mem_chunk_header) + first->size + sizeof(mem_chunk_footer));	//mem location of new header
@@ -93,7 +101,7 @@ void *ma_malloc(size tsize) {
     new_footer->status = new_header->status;
     new_footer->size = new_header->size;
 
-/** Debug*/
+/** Debug
     printf("Size of new header: %d\n", new_header->size);
     printf("Address of new header: %p\n", new_header);
     printf("Size of new footer: %d\n", new_footer->size);
@@ -114,10 +122,54 @@ void ma_free(void *ptr) {
     printf("Free was called\n");
     mem_chunk_header* temp;
     temp = (mem_chunk_header*)((byte*)ptr - sizeof(mem_chunk_header));
-    printf("Address of chunk to free: %p\n", ptr);
-    printf("Address of header to free: %p\n", temp);
+
+    mem_chunk_footer* tempf;
+    tempf = (mem_chunk_footer*)((byte*)temp + temp->size + sizeof(mem_chunk_header));
+
+    //printf("Address of chunk to free: %p\n", ptr);
+    //printf("Address of header to free: %p\n", temp);
 //Set chunk to free
-    temp->status = 1;
+    temp->status = FREE;
+    tempf->status = temp->status;
+//Check next header
+//What if no next header??
+    mem_chunk_header* next;
+    next = (mem_chunk_header*)(((byte*)temp) + sizeof(mem_chunk_header) + temp->size + sizeof(mem_chunk_footer));
+
+    mem_chunk_footer* nextf;
+    nextf = (mem_chunk_footer*)((byte*)next + next->size + sizeof(mem_chunk_header));
+
+    if(next->status==FREE){		//If next chunk is also free they can be merged
+       	//New size is sum of both sizes and also the size of the next header and footer since they can be removed
+	temp->size = temp->size + next->size + sizeof(mem_chunk_header) + sizeof(mem_chunk_footer);
+	nextf->size = temp->size;	//New chunk has current header and next footer
+/**Debug
+	printf("Next header is free\n");
+	printf("New size is %d\n", temp->size);
+	printf("Address of new header: %p\n", temp);
+        printf("Address of new footer: %p\n", nextf);
+	printf("Size in new footer: %d\n", nextf->size);
+/**/	}
+//Check previous header
+    mem_chunk_footer* prevf;
+    prevf = (mem_chunk_footer*)((byte*)temp - sizeof(mem_chunk_footer));
+//    printf("Addr of prevf: %p\n", prevf);
+//    printf("Size of prevf: %d\n", prevf->size);
+
+    mem_chunk_header* prev;
+    prev = (mem_chunk_header*)((byte*)prevf - prevf->size - sizeof(mem_chunk_header));
+//printf("Addr of prev: %p\n", prev);
+
+    if(prevf->status==FREE){		//If previous chunk is also free they can be merged
+	prev->size = prev->size + temp->size + sizeof(mem_chunk_header) + sizeof(mem_chunk_footer);
+	tempf->size = prev->size;
+/**Debug
+	printf("Prev footer is free\n");
+        printf("New size is %d\n", prev->size);
+        printf("Address of new header: %p\n", prev);
+        printf("Address of new footer: %p\n", tempf);	//Problem with footer address
+        printf("Size in new footer: %d\n", tempf->size);
+   /**/ }
 }
 
 /**
@@ -134,23 +186,28 @@ void ma_print(void) {
     }
 }
 
-/** Debug:*/
+/** Debug:
 int main(){
 	ma_init();
-	ma_malloc(10);
-        ma_malloc(15);
+//	ma_malloc(10);
 
 	char* ptr;
         ptr = ma_malloc(20);
         mem_chunk_header* ptr_h;
         ptr_h = (mem_chunk_header*)((byte*)ptr - sizeof(mem_chunk_header));
 
-printf("Allocating 600\n");
-	ma_malloc(600);
-	ma_malloc(10);
+	char* ptr2;
+	ptr2 = ma_malloc(30);
+        mem_chunk_header* ptr2_h;
+        ptr2_h = (mem_chunk_header*)((byte*)ptr2 - sizeof(mem_chunk_header));
+
+//printf("Allocating 600\n");
+//	ma_malloc(600);
+//	ma_malloc(50);
 	ma_free(ptr);
-	ma_malloc(3);
-	ma_malloc(30);
+	ma_free(ptr2);
+	//ma_malloc(35);
+	//ma_malloc(30);
 
 	//ma_print();
 }/**/
